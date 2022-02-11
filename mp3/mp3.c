d02fa7f803e500ebf502df4cc07fef32f12ba75c
Jin Jiang <jinj2@illinois.edu>
2022-02-10 22:01:09 -0600

    Ver 1.0.0

--- mp3/mp3.c
+++ mp3/mp3.c
@@ -1,14 +1,32 @@
+/*group members: jinj2(me)
+ *
+ *
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-  int row;
-
+int main(){
+  int row,k,x,n;
+  unsigned long int result,up;
+  result = 1;
+  x = 0;
+  n = 0;
+  k =1;
   printf("Enter a row index: ");
   scanf("%d",&row);
-
-  // Write your code here
+  n =row;
+  x = row;
+   printf("1 ");
+   x = x - 1;
+  while(x == 0 || x>0){
+    up = n+1-k;
+   result = result * up;
+   result = result/k;
+   printf("%lu ",result);
+   x = x-1;
+   k++;
+}
 
   return 0;
 }
