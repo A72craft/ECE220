cf046e754b3cf9bb52bc744f3f0cf4550f52b7df
Jin Jiang <jinj2@illinois.edu>
2022-03-11 19:35:32 -0600

    finished with comments

--- mp7/sudoku.c
+++ mp7/sudoku.c
@@ -1,5 +1,10 @@
 /*
-
+partner: jinj2(me)
+The 3 "is_val_in" functions are used to see if the given value is feasible. They are
+used in is_val_valid, which is the combnation of the three.
+The cells_full is to check if there are still empty cells. The cells_not_full are used 
+to return coordinate of the first empty value encountered. 
+The solve function is the one that solves the sodoku with backtracking recursives. 
 */
 #include "sudoku.h"
 int is_val_in_row(const int val, const int i, const int sudoku[9][9]);
@@ -7,12 +12,9 @@ int is_val_in_col(const int val, const int j, const int sudoku[9][9]);
 int is_val_in_3x3_zone(const int val, const int i, const int j, const int sudoku[9][9]);
 int is_val_valid(const int val, const int i, const int j, const int sudoku[9][9]);
 int cells_full(const int sudoku[9][9]);
+int cells_not_full_i(const int sudoku[9][9]);
+int cells_not_full_j(const int sudoku[9][9]);
 
-//-------------------------------------------------------------------------------------------------
-// Start here to work on your MP7
-//-------------------------------------------------------------------------------------------------
-
-// You are free to declare any private functions if needed.
 
 // Function: is_val_in_row
 // Return true if "val" already existed in ith row of array sudoku.
@@ -20,8 +22,8 @@ int is_val_in_row(const int val, const int i, const int sudoku[9][9]) {
 
   assert(i>=0 && i<9);
 	for (int j = 0;j<9;j++){
-		if (sudoku[i][j]==val)
-			return 1;} 
+		if (sudoku[i][j]==val) //check every value in the i^th row by incrementing the column
+			return 1;} //return 1 if val already existed
   return 0;
 }
 
@@ -31,8 +33,8 @@ int is_val_in_col(const int val, const int j, const int sudoku[9][9]) {
 
   assert(j>=0 && j<9);
 	for (int i = 0;i<9;i++){
-		if (sudoku[i][j]==val)
-			return 1;} 
+		if (sudoku[i][j]==val) //check every value in the i^th column by incrementing the row
+			return 1;}  //return 1 if val already existed
   return 0;
 
 }
@@ -45,12 +47,12 @@ int is_val_in_3x3_zone(const int val, const int i, const int j, const int sudoku
 	int col,row;
 	col = j/3; //col have 3 cases, 0,1,2, which corresponds to the 3 columns of the 3X3
 	row = i/3; //row have 3 cases, 0,1,2, which corresponds to the 3 rows of 3X3
-  	int new_row = row*3;
-	int new_col = col*3;
+  	int new_row = row*3; 
+	int new_col = col*3; //the new valves now is the upper leftmost cell in the 3X3
 	for (int r = new_row;r<new_row+3;r++){
-		for (int c = new_col;c<new_col+3;c++){
+		for (int c = new_col;c<new_col+3;c++){ //check the value in the 3X3
 			if (sudoku[r][c]==val)
-				return 1;}}
+				return 1;}} //return 1 if val already existed
   return 0;
 
 }
@@ -62,7 +64,7 @@ int is_val_valid(const int val, const int i, const int j, const int sudoku[9][9]
   assert(i>=0 && i<9 && j>=0 && j<9);
 	if (is_val_in_row(val,i,sudoku)==1)
 		return 0;
-	if (is_val_in_col(val,j,sudoku)==1)
+	if (is_val_in_col(val,j,sudoku)==1)  //combines the 3 function into one.return 0 if val already existed.
 		return 0;
 	if (is_val_in_3x3_zone(val,i,j,sudoku)==1)
 		return 0;
@@ -70,37 +72,50 @@ int is_val_valid(const int val, const int i, const int j, const int sudoku[9][9]
   return 1;
 
 }
-
+//function:cells_full
+//check if all the cells are full
 int cells_full(const int sudoku[9][9]){
 	for (int i = 0;i<9;i++){
 		for (int j =0;j<9;j++){
-			if (sudoku[i][j]==0)
-				return 0;}}
+			if (sudoku[i][j]==0) //check if there is a 0.
+				return 0;}}  //if there is a 0, return 0
 	return 1;
 }
-
-int cells_not_full(const int sudoku[9][9]){
+//function:cells_not_full_i
+//return the i (row) value of the first empty cell
+int cells_not_full_i(const int sudoku[9][9]){
+	int ij[2]={0};
 	for (int i = 0;i<9;i++){
 		for (int j =0;j<9;j++){
 			if (sudoku[i][j]==0)
-			return i,j;}}   //It can not pass 2 values. use pointer instead
+			return i;}} 
 }
 
+//function:cells_not_full_j
+//return the j (column) value of the first empty cell
+int cells_not_full_j(const int sudoku[9][9]){
+	int ij[2]={0};
+	for (int i = 0;i<9;i++){
+		for (int j =0;j<9;j++){
+			if (sudoku[i][j]==0)
+			return j;}}
+}
 
 // Procedure: solve_sudoku
 // Solve the given sudoku instance.
 int solve_sudoku(int sudoku[9][9]) {
-	int i,j;
-	if (cells_full(sudoku)==1)
-		return 1;
-	else
-		i,j = cells_not_full(sudoku);
-	for (int val = 1;val<=9;val++){
-		if (is_val_valid(val,i,j,sudoku)==1){
-			sudoku[i][j] = val;
-			if (solve_sudoku(sudoku))
-				return 1;
-			sudoku[i][j] = 0;
+	int i,j; 
+	if (cells_full(sudoku)==1) //check if cell is full
+		return 1;  //start teardown of activation stacks
+	else{
+		i = cells_not_full_i(sudoku);
+		j = cells_not_full_j(sudoku);} //obtain the coordinates
+	for (int val = 1;val<=9;val++){   //for value from 1 to 9
+		if (is_val_valid(val,i,j,sudoku)==1){ //check if the value can be plugged in
+			sudoku[i][j] = val;  //plug it in
+			if (solve_sudoku(sudoku)) //start recursion,if true, continue teardown
+				return 1; 
+			sudoku[i][j] = 0; //if not true,set it back to 0 and try another value
 			}
 	}
 
