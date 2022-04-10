2e33ce4245f794f041a225de13822c7e9555949e
Jin Jiang <jinj2@illinois.edu>
2022-04-09 21:55:48 -0500

    comment,valgrind

--- mp10/sparsemat.c
+++ mp10/sparsemat.c
@@ -5,9 +5,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 void delete_node(sp_tuples * mat_t,int row,int col);
-void sort_node(sp_tuples * mat_t,int row,int col);
+void add_tuples_value(sp_tuples * mat_t, int row, int col, double value);
 
-//void sort_node(sp_tuples * mat_t,int row,int col)
+
 void delete_node(sp_tuples * mat_t,int row,int col){
 	int n,nz;
 	n = mat_t->n;
@@ -18,21 +18,23 @@ void delete_node(sp_tuples * mat_t,int row,int col){
 	sp_tuples_node *nodes_prev = NULL;
 	sp_tuples_node *nodes_next = nodes->next;
 	for(int i = 0;i<nz;i++){
-		compare = nodes->row * n +nodes->col;
+		compare = nodes->row * n + nodes->col;
 		if(i ==0 && compare == index){
 			mat_t->tuples_head = nodes->next;
 			free(nodes);
 			nodes = NULL;
+			mat_t->nz--;
 			break;}
 		else if(compare == index){
 			nodes_prev->next = nodes_next;
 			free(nodes);
 			nodes = NULL;
+			mat_t->nz--;
 			break;}
 		nodes_prev = nodes;
 		nodes = nodes->next;
-		nodes_next = nodes->next;
-		mat_t->nz--;}
+		if(i!= nz-1)
+		nodes_next = nodes->next;}
 		return;
 }
 
@@ -69,8 +71,6 @@ sp_tuples * load_tuples(char* input_file)
 		check = fscanf(f,"%d %d %lf\n",&row,&col,&val);
 		if(check == EOF)
 			break;
-		if(val == 0)
-			continue;
 		set_tuples(tuples, row, col, val);}
 	fclose(f);
     return tuples;
@@ -101,36 +101,46 @@ void set_tuples(sp_tuples * mat_t, int row, int col, double value)
 	n = mat_t->n;
 	nz = mat_t->nz;
 	int index = row * n + col;
-	int compare,first_com;
+	int prev_index = 0;
+	int next_index;
 	sp_tuples_node *nodes = mat_t->tuples_head;
-	sp_tuples_node *nodes_prev = nodes;
-	sp_tuples_node *nodes_next = nodes->next;
-	first_com = nodes->row * n + nodes->col;
+	sp_tuples_node *prev_nodes = NULL;
 	if(value != 0){
 		for(int i = 0;i<=nz;i++){
-			compare = nodes->row * n +nodes->col;
-			if(compare==index){
-				nodes->value = value;}
-			else if(compare<index){
-				sp_tuples_node* new_nodes = malloc(sizeof(sp_tuples_node));
-				nodes_prev->next = new_nodes;
-				new_nodes->row = row;
-				new_nodes->col = col;
-				new_nodes->value = value;
-				new_nodes->next = nodes_next;
-				mat_t->nz++;
+			if(i !=nz)
+			next_index = nodes->row * n + nodes->col;
+			if(index==next_index){
+				nodes->value = value;
 				break;}
-			else if(first_com>index){
-				sp_tuples_node* new_nodes = malloc(sizeof(sp_tuples_node));
-				mat_t->tuples_head = new_nodes;
+			else if(i==0 && index<next_index){
+				sp_tuples_node *new_nodes = malloc(sizeof(sp_tuples_node));
 				new_nodes->row = row;
 				new_nodes->col = col;
 				new_nodes->value = value;
 				new_nodes->next = nodes;
+				mat_t->tuples_head = new_nodes;
 				mat_t->nz++;
-				first_com = new_nodes->row * n +new_nodes->col;
 				break;}
-			nodes_prev = nodes;
+			else if(index>prev_index && index<next_index){
+				sp_tuples_node *new_nodes = malloc(sizeof(sp_tuples_node));
+				new_nodes->row = row;
+				new_nodes->col = col;
+				new_nodes->value = value;
+				new_nodes->next = nodes;
+				prev_nodes->next = new_nodes;
+				mat_t->nz++;
+				break;}
+			else if(i == nz){
+				sp_tuples_node *new_nodes = malloc(sizeof(sp_tuples_node));
+				new_nodes->row = row;
+				new_nodes->col = col;
+				new_nodes->value = value;
+				prev_nodes->next = new_nodes;
+				mat_t->nz++;
+				break;}
+			prev_index = next_index;
+			prev_nodes = nodes;
+			if(i !=nz-1)
 			nodes = nodes->next;}}
 	else if(value == 0){
 		delete_node(mat_t,row,col);}
@@ -156,8 +166,43 @@ void save_tuples(char * file_name, sp_tuples * mat_t)
 
 
 sp_tuples * add_tuples(sp_tuples * matA, sp_tuples * matB){
-
-	return matA;
+	sp_tuples * matC = malloc(sizeof(sp_tuples));
+	sp_tuples_node* nodeA = matA->tuples_head;
+	sp_tuples_node* nodeB = matB->tuples_head;
+	matC->m = matA->m;
+	matC->n = matA->n;
+	matC->nz = 0;
+	for(int i = 0;i< matA->nz;i++){
+		int row,col;
+		double value;
+		row = nodeA->row;
+		col = nodeA->col;
+		value = nodeA->value;
+		if(i==0){
+		sp_tuples_node*	nodeC = malloc(sizeof(sp_tuples_node));
+		nodeC->row = row;
+		nodeC->col = col;
+		nodeC->value = value;
+		matC->tuples_head = nodeC;
+		matC->nz=1;}
+		else{
+			set_tuples(matC,row,col,value);}
+		if(i!=matA->nz-1)
+			nodeA = nodeA->next;}
+	matC->nz = matA->nz;
+	for(int i = 0;i< matB->nz;i++){
+		int row,col;
+		double value;
+		row = nodeB->row;
+		col = nodeB->col;
+		value = nodeB->value;
+		//value = row+col+value;
+		add_tuples_value(matC,row,col,value);
+		if(i!= matB->nz -1 )
+		nodeB = nodeB->next;}
+		
+		
+	return matC;
 }
 
 
@@ -186,7 +231,57 @@ void destroy_tuples(sp_tuples * mat_t){
     return;
 }  
 
-
+void add_tuples_value(sp_tuples * mat_t, int row, int col, double value)
+{
+	int n,nz;
+	n = mat_t->n;
+	nz = mat_t->nz;
+	int index = row * n + col;
+	int prev_index = 0;
+	int next_index;
+	sp_tuples_node *nodes = mat_t->tuples_head;
+	sp_tuples_node *prev_nodes = NULL;
+	if(value != 0){
+		for(int i = 0;i<=nz;i++){
+			if(i !=nz)
+			next_index = nodes->row * n + nodes->col;
+			if(index==next_index){
+				nodes->value = nodes->value+value;
+				break;}
+			else if(i==0 && index<next_index){
+				sp_tuples_node *new_nodes = malloc(sizeof(sp_tuples_node));
+				new_nodes->row = row;
+				new_nodes->col = col;
+				new_nodes->value = value;
+				new_nodes->next = nodes;
+				mat_t->tuples_head = new_nodes;
+				mat_t->nz++;
+				break;}
+			else if(index>prev_index && index<next_index){
+				sp_tuples_node *new_nodes = malloc(sizeof(sp_tuples_node));
+				new_nodes->row = row;
+				new_nodes->col = col;
+				new_nodes->value = value;
+				new_nodes->next = nodes;
+				prev_nodes->next = new_nodes;
+				mat_t->nz++;
+				break;}
+			else if(i == nz){
+				sp_tuples_node *new_nodes = malloc(sizeof(sp_tuples_node));
+				new_nodes->row = row;
+				new_nodes->col = col;
+				new_nodes->value = value;
+				prev_nodes->next = new_nodes;
+				mat_t->nz++;
+				break;}
+			prev_index = next_index;
+			prev_nodes = nodes;
+			if(i !=nz-1)
+			nodes = nodes->next;}}
+	else if(value == 0){
+		return;}
+    return;
+}
 
 
 
